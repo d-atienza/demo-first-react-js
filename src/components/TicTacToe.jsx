35b30958f9c757dd9fece71955307c5e3f9f037d
d-atienza <153103146+d-atienza@users.noreply.github.com>
2024-03-09 17:23:33 +0000

    added reset button

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 export default function TicTacToe() {
+  console.log("this rendered again", new Date());
   const initialCellDataArray = [
     { id: 1, side: "back" },
     { id: 2, side: "back" },
@@ -39,10 +40,26 @@ export default function TicTacToe() {
     }
   });
 
+  function resetGame() {
+    const newCellDataArray = [
+      { id: 1, side: "back" },
+      { id: 2, side: "back" },
+      { id: 3, side: "back" },
+      { id: 4, side: "back" },
+      { id: 5, side: "back" },
+      { id: 6, side: "back" },
+      { id: 7, side: "back" },
+      { id: 8, side: "back" },
+      { id: 9, side: "back" },
+    ];
+
+    setCellDataArray(newCellDataArray);
+  }
+
   return (
     <div>
       <div className="gameGrid">{arrayOfCellElements}</div>
-      <button>reset</button>
+      <button onClick={resetGame}>reset</button>
     </div>
   );
 }
