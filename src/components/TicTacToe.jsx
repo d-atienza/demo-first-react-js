46a330a04d0249c287c6044931ab18d699a36be4
Dani <danichilling@gmail.com>
2024-03-12 16:34:55 +0000

    uncommented out tictactoe

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -1,80 +1,80 @@
-// import { useState } from "react";
+import { useState } from "react";
 
-// export default function TicTacToe() {
-//   console.log("this rendered again", new Date());
-//   const initialCellDataArray = createInitialCellDataArray();
+export default function TicTacToe() {
+  console.log("this rendered again", new Date());
+  const initialCellDataArray = createInitialCellDataArray();
 
-//   const [cellDataArray, setCellDataArray] = useState(initialCellDataArray);
+  const [cellDataArray, setCellDataArray] = useState(initialCellDataArray);
 
-//   function handleCellClicked(idOfCellClicked) {
-//     const newArray = toggleCell(idOfCellClicked, cellDataArray);
-//     setCellDataArray(newArray);
-//   }
+  function handleCellClicked(idOfCellClicked) {
+    const newArray = toggleCell(idOfCellClicked, cellDataArray);
+    setCellDataArray(newArray);
+  }
 
-//   const arrayOfCellElements = cellDataArray.map((food) => {
-//     if (food.side == "back") {
-//       return (
-//         <div
-//           className="cell"
-//           key={food.id}
-//           onClick={() => handleCellClicked(food.id)}
-//         >
-//           🍕
-//         </div>
-//       );
-//     } else {
-//       return (
-//         <div
-//           className="cell"
-//           key={food.id}
-//           onClick={() => handleCellClicked(food.id)}
-//         >
-//           🍔
-//         </div>
-//       );
-//     }
-//   });
+  const arrayOfCellElements = cellDataArray.map((food) => {
+    if (food.side == "back") {
+      return (
+        <div
+          className="cell"
+          key={food.id}
+          onClick={() => handleCellClicked(food.id)}
+        >
+          🍕
+        </div>
+      );
+    } else {
+      return (
+        <div
+          className="cell"
+          key={food.id}
+          onClick={() => handleCellClicked(food.id)}
+        >
+          🍔
+        </div>
+      );
+    }
+  });
 
-//   function resetGame() {
-//     const newCellDataArray = createInitialCellDataArray();
+  function resetGame() {
+    const newCellDataArray = createInitialCellDataArray();
 
-//     setCellDataArray(newCellDataArray);
-//   }
+    setCellDataArray(newCellDataArray);
+  }
 
-//   return (
-//     <div>
-//       <div className="gameGrid">{arrayOfCellElements}</div>
-//       <button onClick={resetGame}>reset</button>
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      <div className="gameGrid">{arrayOfCellElements}</div>
+      <button onClick={resetGame}>reset</button>
+    </div>
+  );
+}
 
-// function createInitialCellDataArray() {
-//   return [
-//     { id: 1, side: "back" },
-//     { id: 2, side: "back" },
-//     { id: 3, side: "back" },
-//     { id: 4, side: "back" },
-//     { id: 5, side: "back" },
-//     { id: 6, side: "back" },
-//     { id: 7, side: "back" },
-//     { id: 8, side: "back" },
-//     { id: 9, side: "back" },
-//   ];
-// }
+function createInitialCellDataArray() {
+  return [
+    { id: 1, side: "back" },
+    { id: 2, side: "back" },
+    { id: 3, side: "back" },
+    { id: 4, side: "back" },
+    { id: 5, side: "back" },
+    { id: 6, side: "back" },
+    { id: 7, side: "back" },
+    { id: 8, side: "back" },
+    { id: 9, side: "back" },
+  ];
+}
 
-// function toggleCell(targetCellID, inputArray) {
-//   const newArray = inputArray.map((obj) => {
-//     if (obj.id === targetCellID) {
-//       return { ...obj, side: getReverseSide(obj.side) };
-//     } else {
-//       return { ...obj };
-//     }
-//   });
+function toggleCell(targetCellID, inputArray) {
+  const newArray = inputArray.map((obj) => {
+    if (obj.id === targetCellID) {
+      return { ...obj, side: getReverseSide(obj.side) };
+    } else {
+      return { ...obj };
+    }
+  });
 
-//   return newArray;
-// }
+  return newArray;
+}
 
-// function getReverseSide(side) {
-//   return side === "front" ? "back" : "front";
-// }
+function getReverseSide(side) {
+  return side === "front" ? "back" : "front";
+}
